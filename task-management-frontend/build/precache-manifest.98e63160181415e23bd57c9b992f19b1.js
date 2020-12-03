bddae13e13cf7e355a4d5ad0e73fa86a27b51c07
Truong <nltruongvi@gmail.com>
2020-12-04 01:02:44 +0700

    Update code

---
+++ task-management-frontend/build/precache-manifest.98e63160181415e23bd57c9b992f19b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b599abf2ef6e4a9548a6b46f7391968",
+    "url": "/index.html"
+  },
+  {
+    "revision": "770fa5683b42cbc124cc",
+    "url": "/static/css/main.d820b323.chunk.css"
+  },
+  {
+    "revision": "96c4fd15eb19b07b7a33",
+    "url": "/static/js/2.bd094128.chunk.js"
+  },
+  {
+    "revision": "770fa5683b42cbc124cc",
+    "url": "/static/js/main.7bf638db.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
