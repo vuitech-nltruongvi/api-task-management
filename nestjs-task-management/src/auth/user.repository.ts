61de040a854eb0c4c93e902ddc78ed2d9ec6d00f
Truong vi <nltruongvi@gmail.com>
2020-12-07 17:59:14 +0700

    Update unit test

--- nestjs-task-management/src/auth/user.repository.ts
+++ nestjs-task-management/src/auth/user.repository.ts
@@ -10,7 +10,7 @@ export class UserRepository extends Repository<User> {
     async signUp(authCredentialsDto: AuthCredentitalsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
 
-        const user = new User();
+        const user = this.create();
 
         user.username = username;
         user.salt = await bcrypt.genSalt();
--- nestjs-task-management/src/example.spec.ts
+++ nestjs-task-management/src/example.spec.ts
@@ -10,11 +10,25 @@ class FriendsList {
     announceFriendship(name) {
         global.console.log(`${name} is now a friend!`)
     }
+
+    removeFriend(name) {
+        const idx = this.friends.indexOf(name)
+
+        if(idx === -1) {
+            throw new Error('Friend not found!');
+        }
+
+        this.friends.splice(idx, 1)
+    }
 }
 
 // tests
 describe('FiendList', () => {
-    const friendsList = new FriendsList()
+    let friendsList = new FriendsList();
+
+    beforeEach(() => {
+        friendsList = new FriendsList();
+    })
 
     it('initializes friends list', () => {
 
@@ -33,5 +47,18 @@ describe('FiendList', () => {
         friendsList.addFriend('Ariel')
 
         expect(friendsList.announceFriendship).toHaveBeenCalledWith('Ariel')
+    })
+
+    describe('removeFriend', () => {
+        it('removes a friend from the list', () => {
+            friendsList.addFriend('Ariel');
+            expect(friendsList.friends[0]).toEqual('Ariel')
+            friendsList.removeFriend('Ariel')
+            expect(friendsList.friends[0]).toBeUndefined()
+        })
+
+        it('throws a error as friend does not exit', () => {
+            expect(() => friendsList.removeFriend('Ariel')).toThrow(new Error('Friend not found!'))
+        })
     })
 })
